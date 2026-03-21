9778ef543171db4bcf1b1f220d187e9337123e9c
zafizing <164548513+zafizing@users.noreply.github.com>
2026-03-21 14:19:14 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
+import dynamic from 'next/dynamic'
+
+const SolanaWalletProvider = dynamic(
+  () => import('../components/WalletProvider'),
+  { ssr: false }
+)
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SolanaWalletProvider>
+      <Component {...pageProps} />
+    </SolanaWalletProvider>
+  )
 }
